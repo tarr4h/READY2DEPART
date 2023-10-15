b4d7a3984707e51193a87942b86acdc5efea3190
tarr4h <81558152+tarr4h@users.noreply.github.com>
2023-10-15 20:37:45 +0900

    FIX : 파일경로 수정, logging 수정

--- src/main/java/com/web/trv/log/interceptor/LogInterceptor.java
+++ src/main/java/com/web/trv/log/interceptor/LogInterceptor.java
@@ -26,23 +26,48 @@ public class LogInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder info = new StringBuilder();
+        info.append("\n =====================================================================================================");
+        info.append("\n == [  REQUEST_URL  ]  ").append(request.getRequestURL());
+        info.append("\n == [  REQUEST_URI  ]  ").append(request.getRequestURI());
+        info.append("\n == [  METHOD       ]  ").append(request.getMethod());
+        info.append("\n == [  REMOTE_IP    ]  ").append(request.getRemoteAddr());
+        info.append("\n == [  ACCEPT       ]  ").append(request.getHeader("accept"));
+        info.append("\n == [  USER_AGENT   ]  ").append(request.getHeader("user-agent"));
+        info.append(getUriRow(request.getRequestURI(), "PROGRESS"));
 
-        sb.append("\n ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
-        sb.append("\n -- requestURI : ").append(request.getRequestURI());
-        sb.append("\n -- requestURL : ").append(request.getRequestURL());
-        sb.append("\n -- method     : ").append(request.getMethod().toString());
-        sb.append("\n -- remote ip  : ").append(request.getRemoteAddr());
-        sb.append("\n -- userAgent  : ").append(request.getHeader("User-Agent"));
-        sb.append("\n ---------------------------------------------------------------------------------------");
-        log.debug(sb.toString());
+        log.debug(info.toString());
+
+//        Enumeration headers = request.getHeaderNames();
+//        while(headers.hasMoreElements()){
+//            String name = (String) headers.nextElement();
+//            log.debug("\n name = {}, value = {}", name, request.getHeader(name));
+//        }
 
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        log.debug("\n ------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        String info = "";
+        info += getUriRow(request.getRequestURI(), "TERMINATE");
+        info += "\n =====================================================================================================";
+        log.debug(info);
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    public String getUriRow(String requestUri, String text){
+        StringBuilder info = new StringBuilder();
+        info.append("\n --------------------------------");
+        info.append(" [ ").append(text);
+        info.append(" ".repeat(10 - text.length()));
+        info.append(" | ");
+        info.append(requestUri);
+        info.append(" ] ");
+        int uriLen = requestUri.length();
+        int dashLen = "--------------------------------------------------".length();
+        info.append("-".repeat(Math.max(0, dashLen - uriLen)));
+
+        return info.toString();
     }
 }
